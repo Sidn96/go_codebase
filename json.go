0cfedec866209dffb9f50c33bd27a97a886362e9
siddhesh <54312356+Sidn96@users.noreply.github.com>
2023-07-06 20:55:42 +0530

    error code response changes

--- json.go
+++ json.go
@@ -7,27 +7,28 @@ import (
 )
 
 func respondWithError(w http.ResponseWriter, code int, msg string) {
-	if (code > 499) {
+	if code > 499 {
 		log.Println("Responding with 5XX error", msg)
-		type errResponse struct {
-			Error string `json:"error"`
-		}
-
-		respondWithJson(w, code, errResponse {
-			Error: msg,
-		})
 	}
+
+	type errResponse struct {
+		Error string `json:"error"`
+	}
+
+	respondWithJson(w, code, errResponse{
+		Error: msg,
+	})
 }
 
 func respondWithJson(w http.ResponseWriter, code int, payload interface{}) {
 	dat, err := json.Marshal(payload)
 
-	if (err != nil) {
+	if err != nil {
 		log.Printf("Failed to Marshall json response %v", payload)
 		w.WriteHeader(500)
 		return
 	}
 	w.Header().Add("Content-Type", "application/json")
-	w.WriteHeader(200)
+	w.WriteHeader(code)
 	w.Write(dat)
 }
